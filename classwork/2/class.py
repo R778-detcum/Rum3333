d32562225d76f33f66ab71e7e9822518d4c592ec
Rumeysa Erbey <rumpet333@gmail.com>
2025-09-20 11:10:15 -0700

    классная работа

---
+++ classwork/2/class.py
@@ -0,0 +1,23 @@
+#1 задача
+# lskh=list(range(1,21))
+# print(lskh)
+
+#2 задача
+# numbers=[1,2,3,4,5]
+# total_sum=sum(numbers)
+# total_product=1
+# for num in numbers:
+#     total_product*= num
+# print(f"Cумма: {total_sum}")
+# print(f"Произведение: {total_product}")
+
+# 3 задача
+# numb= [1,2,3,4,5,6,7]
+# chetni=[]
+#
+# for num in numb:
+#     if num % 2 == 0 :
+#         chetni.append(num)
+# print(chetni)
+
+#4 задача
